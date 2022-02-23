6f1587247d10a13b9ce0698df94afd00882ca4c0
pbores001 <58749676+pbores001@users.noreply.github.com>
2022-02-23 22:58:36 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+#Let's do some pixel art
+
+###Simple JavaScript application to do pixel art pictures
+
+*See it in action here https:   👈
+
---
+++ custom.js
@@ -0,0 +1,10 @@
+var pixelColor = 'green';
+
+function setPenColor(pen) {
+    pixelColor = pen;
+}
+
+
+function setPixelColour(pixel) {
+    pixel.style.backgroundColor = pixelColor;
+}
